bdad658433564938afbcaeffef51a72aa710f646
MMarek <mildnermarek@gmail.com>
2020-02-14 13:58:44 +0100

    slider image index

--- task03.js
+++ task03.js
@@ -5,10 +5,28 @@ document.addEventListener('DOMContentLoader', function () {
     const buttonPrevious = document.getElementById('.prevPicture');
     console.log(buttonPrevious);
     const allListElements = document.querySelectorAll('li');
-
+    console.log(allListElements);
     let imageIndex = 0;
 
     allListElements[imageIndex].classList.add('visible');
 
+    buttonNext.addEventListener('click', function () {
+        allListElements[imageIndex].classList.remove('visible');
 
+        imageIndex++;
+        if (imageIndex > allListElements.length - 1) {
+            imageIndex = 0;
+        }
+        allListElements[imageIndex].classList.add('visible');
+    });
+
+    buttonPrevious.addEventListener('click', function () {
+        allListElements[imageIndex].classList.remove('visible');
+
+        imageIndex--;
+        if (imageIndex < 0) {
+            imageIndex = allListElements.length - 1;
+        }
+        allListElements[imageIndex].classList.add('visible');
+    });
 });
