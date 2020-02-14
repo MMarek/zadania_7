87e03ca4f6449c32fb37da69ad4d5d941a1d262b
MMarek <mildnermarek@gmail.com>
2020-02-14 14:31:56 +0100

    slider image index correction

--- task03.js
+++ task03.js
@@ -1,32 +1,29 @@
-document.addEventListener('DOMContentLoader', function () {
+const buttonNext = document.querySelector('.nextPicture');
+console.log(buttonNext);
+const buttonPrevious = document.querySelector('.prevPicture');
+console.log(buttonPrevious);
+const allListElements = document.querySelectorAll('li');
+console.log(allListElements);
+let imageIndex = 0;
 
-    const buttonNext = document.getElementById('.nextPicture');
-    console.log(buttonNext);
-    const buttonPrevious = document.getElementById('.prevPicture');
-    console.log(buttonPrevious);
-    const allListElements = document.querySelectorAll('li');
-    console.log(allListElements);
-    let imageIndex = 0;
+allListElements[imageIndex].classList.add('visible');
 
+buttonNext.addEventListener('click', function () {
+    allListElements[imageIndex].classList.remove('visible');
+
+    imageIndex++;
+    if (imageIndex > allListElements.length - 1) {
+        imageIndex = 0;
+    }
     allListElements[imageIndex].classList.add('visible');
+});
 
-    buttonNext.addEventListener('click', function () {
-        allListElements[imageIndex].classList.remove('visible');
+buttonPrevious.addEventListener('click', function () {
+    allListElements[imageIndex].classList.remove('visible');
 
-        imageIndex++;
-        if (imageIndex > allListElements.length - 1) {
-            imageIndex = 0;
-        }
-        allListElements[imageIndex].classList.add('visible');
-    });
-
-    buttonPrevious.addEventListener('click', function () {
-        allListElements[imageIndex].classList.remove('visible');
-
-        imageIndex--;
-        if (imageIndex < 0) {
-            imageIndex = allListElements.length - 1;
-        }
-        allListElements[imageIndex].classList.add('visible');
-    });
+    imageIndex--;
+    if (imageIndex < 0) {
+        imageIndex = allListElements.length - 1;
+    }
+    allListElements[imageIndex].classList.add('visible');
 });
