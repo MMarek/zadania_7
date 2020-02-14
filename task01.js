db67ef2f6910ee4036866d760256e1baebfc0b34
MMarek <mildnermarek@gmail.com>
2020-02-14 11:52:56 +0100

    agree

--- task01.js
+++ task01.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const formElement = document.querySelector('form');
-    const errorsList = document.querySelector('.errors-message');
+    const errorsList = document.querySelector('.error-message');
 
-    formElement.addEventListener('submit',function (e) {
-        console.log('submit', this.elements);
+    formElement.addEventListener('submit', function (e) {
+        e.preventDefault();
 
         const errors = [];
-
         const emailVal = this.elements.email.value;
         if (emailVal.indexOf('@') === -1) {
             errors.push('Email musi posiadać znak @');
+        }
+
+        const nameVal = this.elements.name.value;
+        if (nameVal.length < 6) {
+            errors.push('Imię za krótkie')
+        }
+
+        const agreeVal = this.elements.agree.checked;
+        if (!agreeVal) {
+            errors.push('Zaakceptuj warunki');
         }
 
         if (errors.length > 0) {
