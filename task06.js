f782213e431ea7460e6c07bb632a79f175a1ce4a
MMarek <mildnermarek@gmail.com>
2020-02-16 16:04:47 +0100

    menu

---
+++ task06.js
@@ -0,0 +1,21 @@
+const listElem = document.querySelectorAll('div > ul > li');
+console.log(listElem);
+for (let i = 4; i < listElem.length; i++) {
+    listElem[i].addEventListener('click', function () {
+        let nestedList = this.querySelector('ul');
+        let nestedListElem = this.querySelectorAll('ul > li');
+
+        if (nestedListElem.length > 0) {
+            nestedList.style.display = 'block';
+        }
+    });
+
+    listElem[i].addEventListener('click', function () {
+        let nestedList = this.querySelector('ul');
+        let nestedListElem = this.querySelectorAll('ul > li');
+
+        if (nestedListElem.length > 0) {
+            nestedList.style.display = 'none';
+        }
+    });
+}
